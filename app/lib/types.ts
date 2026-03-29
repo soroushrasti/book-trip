9783561975da011871ec51747b25cba0c432a6d7
Soroush Rasti <rasti.soroush@gmail.com>
2026-03-29 23:41:18 +0200

    Add cache-busting version to images to force Railway refresh

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -39,22 +39,25 @@ export type SessionData = {
 
 export const MAX_CAPACITY = 15;
 
+// Cache buster version - increment to force image refresh
+const IMG_VERSION = "v2";
+
 export const SESSIONS: SessionInfo[] = [
   {
     id: "2026-04-25",
     date: "2026-04-25",
     book: "De mooiste vis van de zee",
     group: "Groep 1-2",
-    image: "/group_1_2_april.jpeg",
+    image: `/group_1_2_april.jpeg?${IMG_VERSION}`,
     time: "11:45 - 14:15",
     maxCapacity: MAX_CAPACITY,
   },
   {
     id: "2026-05-23",
     date: "2026-05-23",
-    book: "Billy the Bat's big adventure",
+    book: "Billy the Bat's Big Adventure",
     group: "Groep 1-2",
-    image: "/group_1_2may.jpeg",
+    image: `/group_1_2may.jpeg?${IMG_VERSION}`,
     time: "11:45 - 14:15",
     maxCapacity: MAX_CAPACITY,
   },
@@ -63,7 +66,7 @@ export const SESSIONS: SessionInfo[] = [
     date: "2026-08-22",
     book: "Draw me a star",
     group: "Groep 3-4",
-    image: "/grouo_3_4_agust.jpeg",
+    image: `/grouo_3_4_agust.jpeg?${IMG_VERSION}`,
     time: "11:45 - 14:15",
     maxCapacity: MAX_CAPACITY,
   },
@@ -72,7 +75,7 @@ export const SESSIONS: SessionInfo[] = [
     date: "2026-09-19",
     book: "The tiny seed",
     group: "Groep 5-6",
-    image: "/group_5_6_septamber.jpeg",
+    image: `/group_5_6_septamber.jpeg?${IMG_VERSION}`,
     time: "11:45 - 14:15",
     maxCapacity: MAX_CAPACITY,
   },
@@ -81,7 +84,7 @@ export const SESSIONS: SessionInfo[] = [
     date: "2026-10-17",
     book: "Flotsam",
     group: "Groep 7-8",
-    image: "/group_7_8_oct.jpeg",
+    image: `/group_7_8_oct.jpeg?${IMG_VERSION}`,
     time: "11:45 - 14:15",
     maxCapacity: MAX_CAPACITY,
     isExhibition: true,
